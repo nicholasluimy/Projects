5c579c5c0e5db86c810d5f40e04f0843af3cacdf
nicholasluimy <nicholasluimy@gmail.com>
2016-04-07 21:09:13 +0800

    find prime factors

---
+++ Numbers/find_prime_factors.py
@@ -0,0 +1,17 @@
+def is_prime(n):
+    for i in range(2, n//2):
+        if n%i == 0:
+            return False
+    return True
+
+def prime_factors(n):
+    final = []
+    for i in range(2,(n//2)+1):
+        
+        if n%i == 0:
+            
+            if is_prime(i):
+                
+                
+                final.append(i)
+    return final
